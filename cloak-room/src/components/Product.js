b781f1e8156469ae5cc48ddee019072be7680ea1
Aaron <wil16001766@btc.ac.uk>
2022-03-31 13:56:50 +0100

    component creations

---
+++ cloak-room/src/components/Product.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Container, Button} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+
+function Product({product}) {
+    return (
+        <Container className="m-2">
+            <h3>{product.name}</h3>
+            <img className="w-100" src={product.image} alt={product.name} />
+            <h4>£{product.price}</h4>
+            <p>{product.description}</p>
+            <Button as={Link} to={`/menu/${product.id}`} className="d-block w-100 my-2" variant="light">View {product.name}</Button>
+            <Button className="d-block w-100 my-2" variant="primary">Add to basket</Button>
+        </Container>
+    )
+}
+
+
+export default Product
